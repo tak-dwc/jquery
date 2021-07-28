a4dacdbeedc755a047873daea8d68a23d43dd781
tak-dwc <taku3277@gmail.com>
2021-07-28 23:35:34 +0900

    [start]jwuery

---
+++ app.js
@@ -0,0 +1,32 @@
+// $(document).ready(function () {
+//   $('body').html('<h1>Hello jQuery!!</h1>');
+// });
+
+// $(function(){
+//  $('.box1').css({
+//   'background-color':'#0000FF',
+//   'height':'100px'
+//  });
+// });
+
+// $(function(){
+//   $('.box1').show();
+//   $('.box1').css({'background-color':'#0000FF'});
+// });
+
+
+// $(function(){
+//   $('.box1').hide();
+  
+// });
+
+$(function(){
+ $('.box1').slideDown(function() {
+   $('.box1').css({
+  'background-color':'#0000FF',
+  'height':'100px',
+   'width':'200px'
+  }).slideUp(5000);
+ });
+});
+
